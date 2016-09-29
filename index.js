b58f97a4f1087573ad7b0ae89176c51f20fd64b5
Jordomav <jordomav@gmail.com>
2016-09-28 22:48:25 -0700

    added endpoints for google images and wiki

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
+require('dotenv').config();
 var express = require('express');
 var app = express();
 var open = require('openurl');
 
-app.get('/show', function(req, res) {
+app.get('/google', function(req, res) {
     var display = req.param('display');
     console.log(display);
     res.send('Showing the user '  + display);
     open.open('https://www.google.com/search?q=' + display);
+});
+
+app.get('/wiki', function(req, res) {
+    var display = req.param('display');
+    console.log(display);
+    res.send('Showing the user '  + display);
+    open.open('https://en.wikipedia.org/wiki/' + display);
+});
+
+app.get('/image', function(req, res) {
+    var display = req.param('display');
+    console.log(display);
+    res.send('Showing the user '  + display);
+    open.open('https://www.google.com/search?hl=en&site=imghp&tbm=isch&source=hp&biw=1279&bih=676&q=' + display);
 });
 
 app.listen(3000, function () {
